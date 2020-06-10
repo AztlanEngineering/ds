f483fae909400fc90cec434e529a88f5c3bfbe22
779g <merida@779.mx>
2020-06-10 16:12:41 +0000

    Icon Card

---
+++ src/stories/site/IconCard.stories.js
@@ -0,0 +1,46 @@
+/* @fwrlines/generator-storybook-story 1.5.2 */
+import * as React from 'react'
+//import {} from 'react'
+
+//import { action } from '@storybook/addon-actions'
+
+import { IconCard } from 'ui'
+/* import QUERY from './graphql/query.graphql'
+   import { AplProvider } from 'stories/utils'
+   import { Router } from 'stories/utils'
+   import {ALL_COLORS, SIZES } from 'stories/config.js'
+   import { LIST, LIST_XS, TEXT_XXS_ESC, TEXT_XXS, TEXT_XS, TEXT } from 'stories/utils/Dummy' */
+
+//const endpoint = 'https://api.fwrlines.com/graphql'
+
+export default {
+  title        :'site/IconCard',
+  component    :IconCard,
+  //componentSubtitle:'Component subtitle',
+  subcomponents:{
+    //Item:IconCard.Item
+  },
+  parameters:{
+    decorators:[
+      /* storyfn => <div className="">{ storyfn() }</div>,
+         storyfn => <AplProvider endpoint={ endpoint }>{ storyfn() }</AplProvider>,
+         storyfn => <Router>{ storyfn() }</Router>, */
+    ]
+  }
+}
+
+export const Default = () => (
+  <IconCard
+
+    headingProps={{
+
+      heading         :'Hello its me the title',
+      headingAs       :'span',
+      headingClassName:'h4 s3 k-s',
+      children        :<p className='s0 k-s'>One of the features explained</p>
+    }}
+  >
+  </IconCard>
+)
+
+
---
+++ src/ui/site/IconCard/IconCard.js
@@ -0,0 +1,155 @@
+/* @fwrlines/generator-react-component 2.3.3 */
+import * as React from 'react'
+//import {} from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  SVG
+} from 'ui/common'
+
+import {
+  Card ,
+  Heading,
+} from 'ui/elements'
+
+//Intl
+
+/* import { FormattedMessage} from "react-intl";
+   import messages from "./messages";
+    <FormattedMessage {...messages.title} /> */
+
+//Config
+
+//import C from 'ui/cssClasses'
+
+/* Relative imports
+   import styles from './icon_card.scss' */
+import { isBackend } from 'ui/isBackend'
+
+if(!isBackend) {
+  import('./icon_card.scss')
+}
+
+const baseClassName = 'icon_card'
+
+
+/**
+ * Use `IconCard` to
+ * Has color `x`
+ */
+const IconCard = ({
+  id,
+  className,
+  style,
+
+  svgHeight,
+  svgWidth,
+  svgSprite,
+  svgTarget,
+  svgClassName,
+
+  headingProps
+}) => {
+
+
+  return (
+    <Card
+      className={
+        [
+        //styles[baseClassName],
+          baseClassName,
+          className
+        ].filter(e => e).join(' ')
+      }
+      id={ id }
+      style={ style }
+    >
+      <Card.Section>
+        <SVG
+          height={ svgHeight }
+          width={ svgWidth }
+          target={ svgTarget }
+          sprite={ svgSprite }
+          className={ svgClassName }
+        />
+      </Card.Section>
+      <Card.Section>
+        <Heading
+          { ...headingProps }
+        >
+        </Heading>
+      </Card.Section>
+    </Card>
+  )}
+
+IconCard.propTypes = {
+  /**
+   * Provide an HTML id to this element
+   */
+  id:PropTypes.string,
+
+  /**
+   * The html class names to be provided to this element
+   */
+  className:PropTypes.string,
+
+  /**
+   * The JSX-Written, css styles to apply to the element.
+   */
+  style:PropTypes.object,
+
+  /**
+   *  The children JSX
+   */
+  children:PropTypes.node,
+
+  /**
+   * Which html tag to use
+   */
+  as:PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object
+  ]),
+  //as: PropTypes.string,
+
+  /**
+   * The height of the `SVG`
+   */
+  svgHeight:PropTypes.string,
+
+  /**
+   * The width of the `SVG`
+   */
+  svgWidth:PropTypes.string,
+
+  /**
+   * The target of the `SVG`
+   */
+  svgTarget:PropTypes.string,
+
+  /**
+   * The target of the `SVG`
+   */
+  svgSprite:PropTypes.string,
+
+  /**
+   * The className of the `SVG`
+   */
+  svgClassName:PropTypes.string,
+
+  /**
+   * The props passed to `Heading`
+   */
+  headingProps:PropTypes.object,
+}
+
+IconCard.defaultProps = {
+  svgHeight   :'6em',
+  svgWidth    :'6em',
+  svgTarget   :'engine',
+  svgClassName:'x-primary',
+  /* height:'2.2em',
+     as:'p', */
+}
+
+export default IconCard
